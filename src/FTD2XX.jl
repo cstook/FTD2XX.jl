fd05296f4396ad8902992be62774a6945a4a80c5
Chris Stook <chris@chrisstook.com>
2015-10-06 16:21:07 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/FTD2XX.jl
+++ src/FTD2XX.jl
@@ -5,6 +5,6 @@ include("checkstatus.jl")
 include("common_API.jl")
 @linux? include("osx_linux_API.jl"):nothing
 @osx? include("osx_linux_API.jl"):nothing
-@windows? include("windows_API.jl") : nothing
+@windows? include("windows_API.jl"):nothing
 
 end # module
