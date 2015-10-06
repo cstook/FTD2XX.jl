b9cd361f633f9e4bb1f792eb18a8103093437be0
Chris Stook <chris@chrisstook.com>
2015-10-06 12:44:29 -0400

    seperated operating system dependent functions
    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/FTD2XX.jl
+++ src/FTD2XX.jl
@@ -3,7 +3,8 @@ module FTD2XX
 
 include("checkstatus.jl")
 include("common_API.jl")
-
-
+@linux? include("osx_linux_API.jl"):nothing
+@osx? include("osx_linux_API.jl"):nothing
+@windows? include("windows_API.jl") : nothing
 
 end # module
