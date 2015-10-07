f00b4d470cab537ec8869ed7c1aa07d1a2f4bc9d
Chris Stook <chris@chrisstook.com>
2015-10-07 14:40:51 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/FTD2XX.jl
+++ src/FTD2XX.jl
@@ -1,6 +1,8 @@
 #__precompile__()
 module FTD2XX
 
+include("getFTD2XX.jl")
+
 include("checkstatus.jl")
 include("common_API.jl")
 @linux? include("osx_linux_API.jl"):nothing
--- src/getFTD2XX.jl
+++ src/getFTD2XX.jl
@@ -2,7 +2,7 @@
 # src directory.  If not get correct driver
 # from FTDI website
 
-using ZipFile
+@windows? using ZipFile : nothing
 
 function haveftd2xx()
   @windows? isfile("ftd2xx.dll"):false
@@ -13,9 +13,9 @@ function getftd2xx()
     driver = download("http://www.ftdichip.com/Drivers/CDM/CDM v2.12.06 WHQL Certified.zip")
     r = ZipFile.Reader(driver)
     indexofdll = findfirst(x->x.name=="i386/ftd2xx.dll",r.files)
-    uncompresseszize = r.files[indexofdll].uncompressedsize
-    dllbytearray = Array(UInt8,uncompressedfilesize)
-    read(r.files[index],dllbytearray)
+    uncompressedsize = r.files[indexofdll].uncompressedsize
+    dllbytearray = Array(UInt8,uncompressedsize)
+    read(r.files[indexofdll],dllbytearray)
     io = open("ftd2xx.dll","w")
     write(io,dllbytearray)
     close(io)
