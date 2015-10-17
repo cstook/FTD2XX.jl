aea30920833fa8e64df8d2d802e8a5be10eda676
Chris Stook <chris@chrisstook.com>
2015-10-17 18:34:24 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- src/FT_Read.jl
+++ src/FT_Read.jl
@@ -1,12 +1,13 @@
-export FT_Read
+export FT_Read!
 
-function FT_Read(ft_handle::Culong, bytestoread::Integer)
-  buffer = Array(UInt8,bytestoread)
+function FT_Read!(ft_handle::Culong,
+                 buffer::Array{UInt8},
+                 bytestoread::Integer)
   bytesreturned = Ref{Cuint}()
   ft_status = ccall((:FT_Read, d2xx),
                       Cuint,
                       (Culong, Ptr{UInt8}, Cuint, Ref{Cuint}),
                       ft_handle, buffer, bytestoread, bytesreturned)
   checkstatus(ft_status) 
-  return (bytesreturned[], buffer[1:bytesreturned[]])
+  return (bytesreturned[])
 end
